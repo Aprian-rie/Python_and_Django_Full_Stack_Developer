a5cd00b2eb1cb137cf5400d12208d303969c4678
Apryan27 <aprythomas5@gmail.com>
2023-10-02 09:09:49 +0300

    Functions in JS

--- Front-End/JS_level_one/part9/script.js
+++ Front-End/JS_level_one/part9/script.js
@@ -1,17 +1,46 @@
-var fName = "Jose"
-var lName = "Johnson"
-var age  = "26"
-var height = "175"
-var petName = "Sammy"
-
 var fNamePrompt = prompt("Enter Your First Name")
 var lNamePrompt = prompt("What is your Last Name")
 var agePrompt= prompt("What is your age")
 var heightPrompt = prompt("What is your height")
 var petNamePrompt = prompt("What is your pet Name")
 
-if (fName === fNamePrompt  && lName === lNamePrompt  && age === agePrompt && height === heightPrompt && petName === petNamePrompt) {
-  console.log("Welcome Comrade! You've passed the Spy Test");
+alert("Thank you so much for your Information")
+
+// Four conditions
+
+
+
+
+
+var fNameCond = false
+var lNameCond = false
+var ageCond  = false
+var heightCond = false
+var petNameCond = false
+
+// Name condition
+
+if (fNamePrompt[0] === lNamePrompt[0]) {
+  fNameCond = true
+}
+
+// Age condition
+if (agePrompt > 20 && agePrompt < 30) {
+  ageCond = true
+}
+
+// Height condition
+if (heightPrompt >= 170) {
+  heightCond = true
+}
+
+// Pet condition
+if (petNamePrompt[petNamePrompt.length - 1] === "y") {
+  petNameCond = true
+}
+
+if (fNameCond && ageCond && heightCond && petNameCond){
+  console.log("Welcome Spy !!");
 }else {
-  console.log("Sorry, nothing to see here");
+  console.log("Pass along");
 }
---
+++ Front-End/JS_level_two/README.md
@@ -0,0 +1,18 @@
+## JavaScript Level Two
+So far we covered only the basics of Javascript, time for some advanced concepts
+
+I'll be covering 
+
+- Functions
+- Arrays
+- Objects
+
+### Functions
+We use the function keyword to indicate that we have a function
+The General sytax for a function
+
+```commandline
+function name(parameter1, parameter2){
+    // code to be executed
+}
+```
